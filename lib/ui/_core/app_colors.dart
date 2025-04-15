5cb10a59a765f788fceb950c4f70584c1dd10636
alxlimaworks <alxlimaworks@gmail.com>
2025-04-15 20:33:51 +0000

    Aula 01- Seu app começa aqui, monte a base e a primeira tela

---
+++ lib/ui/_core/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// cares padrões de instancia de cores das telas.
+abstract class AppColors {
+  static const Color backgraundColor = Color(0xFF202123);
+  static const Color mainColor = Color(0xFFffa559);
+}
